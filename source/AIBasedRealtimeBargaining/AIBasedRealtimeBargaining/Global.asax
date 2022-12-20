5ef3b88c05d525c274710a0b87cf8411b3d23d4b
Soubhik Nandy <soubhik.nandy@indusnet.co.in>
2022-12-20 19:04:02 +0530

    Initial Commit

---
+++ source/AIBasedRealtimeBargaining/AIBasedRealtimeBargaining/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AIBasedRealtimeBargaining.Global" Language="C#" %>
---
+++ source/AIBasedRealtimeBargaining/AIBasedRealtimeBargaining/Logic/GenericLogic.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{
+	public class GenericLogic
+	{
+		public static DateTime IstNow
+		{
+			get
+			{
+				return System.DateTime.UtcNow.AddHours(5.5);
+			}
+		}
+	}
+}
